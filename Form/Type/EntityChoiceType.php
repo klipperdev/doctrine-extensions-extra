1a40a29591efb933889ba63769604f51b3561040
François Pluchino <francois.pluchino@klipper.dev>
2021-09-09 09:22:54 +0200

    Add form type for entity choice

---
+++ Form/Type/EntityChoiceType.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\DoctrineExtensionsExtra\Form\Type;
+
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\Form\Doctrine\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Doctrine Entity Choice form type.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class EntityChoiceType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'class' => ChoiceInterface::class,
+            'query_builder' => static function (Options $options) {
+                return $options['em']->getRepository($options['class'])->createQueryBuilder('c')
+                    ->andWhere('c.type = :type')
+                    ->setParameter('type', $options['type'])
+                ;
+            },
+        ]);
+
+        $resolver->setRequired([
+            'type',
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return EntityType::class;
+    }
+}
